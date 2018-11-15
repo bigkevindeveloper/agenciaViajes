3d747ba319be74cc51f6f7438093d668aa872d95
kevin rivera <kevinriveradeveloper@gmail.com>
2018-11-15 13:04:06 +0100

    Fix: header, Fix: titulo destinos

--- public_html/js/config-dom.js
+++ public_html/js/config-dom.js
@@ -6,11 +6,13 @@ $(document).ready(function(){
  	/*Sticky menu*/
  	var altura = $('#menu-navegacion').offset().top;
 	
+
 	$(window).on('scroll', function(){
 		if ( $(window).scrollTop() > altura+100 ){
 			$('#menu-navegacion').addClass('menu-fixed');
 			$('.titulo').css({"color":"#ffffff"});
 			$('#logo').attr('src', 'md/iconoweb2.png');
+			$('header').css({"height":"15vh"});
 		} else {
 			$('#menu-navegacion').removeClass('menu-fixed');
 			$('.titulo').css({"color":"#000000"});
@@ -18,7 +20,38 @@ $(document).ready(function(){
 		}
 	});
 });
+/*Funciones de navegacion*/
+/*Scroll a secciones*/
 
+$('#btn-destinos').click(function(){
+	$('html, body').animate({
+        // #elemento2 · Será el elemento donde queremos desplazarnos.
+    	scrollTop: $("#sec-destinos").offset().top
+        //1500 · La velocidad que le queremos dar
+	 	}, 1800);
+})
+
+$('#btn-quien').click(function(){
+	$('html, body').animate({
+        // #elemento2 · Será el elemento donde queremos desplazarnos.
+    	scrollTop: $("#sec-quienessomos").offset().top
+        //1500 · La velocidad que le queremos dar
+	 	}, 1800);
+})
+$('#btn-donde').click(function(){
+	$('html, body').animate({
+        // #elemento2 · Será el elemento donde queremos desplazarnos.
+    	scrollTop: $("#sec-contacto").offset().top
+        //1500 · La velocidad que le queremos dar
+	 	}, 1800);
+})
+$('#btn-home').click(function(){
+	$('html, body').animate({
+        // #elemento2 · Será el elemento donde queremos desplazarnos.
+    	scrollTop: $("#sec-inicio").offset().top
+        //1500 · La velocidad que le queremos dar
+	 	}, 1800);
+})
 /*Calcular el alto de la pantalla*/
 function configurarCSS(screen){
 	$(".full-page").css({"min-height":screen});
