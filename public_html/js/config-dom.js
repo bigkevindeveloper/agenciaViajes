66c907047abd0d2fff096d00c7af8fb93b65f1db
kevin rivera <kevinriveradeveloper@gmail.com>
2018-11-21 16:14:28 +0100

    responsive:header,menu,home,logo

--- public_html/js/config-dom.js
+++ public_html/js/config-dom.js
@@ -4,23 +4,64 @@ $(document).ready(function(){
  	configurarCSS(height);
 
  	/*Sticky menu*/
- 	var altura = $('#menu-navegacion').offset().top;
-	
-
+    var altura = $('#menu-navegacion').offset().top;
+    var d = $(window).scrollTop();
+    if ($(window).scrollTop() > 100) {
+        configurarMenu();
+    }
 	$(window).on('scroll', function(){
-		if ( $(window).scrollTop() > altura+100 ){
+		if ( $(window).scrollTop() > 150 ){
 			$('#menu-navegacion').addClass('menu-fixed');
 			$('.titulo').css({"color":"#ffffff"});
 			$('#logo').attr('src', 'md/iconoweb2.png');
 			$('header').css({"height":"15vh"});
-		} else {
+        }
+        if ($(window).scrollTop() < 150){
 			$('#menu-navegacion').removeClass('menu-fixed');
 			$('.titulo').css({"color":"#000000"});
 			$('#logo').attr('src', 'md/iconoweb.png');
 		}
-	});
+    });
+    /*Escuchador de actualizaciones de pagina*/
+    $(window).on("beforeunload", function (e) {
+        console.log("actualizacion");
+    });
+    function configurarMenu() {
+        $('#menu-navegacion').addClass('menu-fixed');
+        $('.titulo').css({ "color": "#ffffff" });
+        $('#logo').attr('src', 'md/iconoweb2.png');
+        $('header').css({ "height": "15vh" });
+    }
+    function configurarMenu2() {
+        if ($(window).scrollTop() > 150) {
+            $('#menu-navegacion').addClass('menu-fixed');
+            $('.titulo').css({ "color": "#ffffff" });
+            $('#logo').attr('src', 'md/iconoweb2.png');
+            $('header').css({ "height": "15vh" });
+        }
+    }
+    //Configuramos el menu desplegable.
+    var estadoMenu = false; // Estados: ->False(cerrado) & True(Abierto)
+    $('#burguer').click(function () {
+        configurarMenu2();
+        if (!estadoMenu) {
+            /*Lo que ejecutara si el menu es visible*/
+            estadoMenu = true;
+            /*Vamos a aplicarle la propiedad visible a nuestro menu desplegable*/
+            $('#menu_desplegado').attr('class', 'd-block menu-desplegado col-7');
+            console.log(estadoMenu);
+        } else {
+            /*Lo que se ejecutara si no es visible*/
+            $('#menu_desplegado').attr('class', 'd-none menu-desplegado col-7');
+            estadoMenu = false;
+            console.log(estadoMenu);
+        }
+    })
 });
 /*Funciones de navegacion*/
+function ocultarMenu() {
+    $("#burguer")[0].click();
+}
 /*Scroll a secciones*/
 
 $('#btn-destinos').click(function(){
@@ -52,7 +93,32 @@ $('#btn-home').click(function(){
         //1500 · La velocidad que le queremos dar
 	 	}, 1800);
 })
-/*Calcular el alto de la pantalla*/
+/*botones burguer*/
+$('#bb-inicio').click(function () {
+    ocultarMenu();
+        $('html, body').animate({
+            // #elemento2 · Será el elemento donde queremos desplazarnos.
+            scrollTop: $("#sec-inicio").offset().top - 90
+            //1500 · La velocidad que le queremos dar
+        }, 1800);
+    })
+$('#bb-viajes').click(function () {
+    ocultarMenu();
+        $('html, body').animate({
+            // #elemento2 · Será el elemento donde queremos desplazarnos.
+            scrollTop: $("#sec-destinos").offset().top - 90
+            //1500 · La velocidad que le queremos dar
+        }, 1800);
+    })
+$('#bb-qs').click(function () {
+    ocultarMenu();
+        $('html, body').animate({
+            // #elemento2 · Será el elemento donde queremos desplazarnos.
+            scrollTop: $("#sec-quienessomos").offset().top
+            //1500 · La velocidad que le queremos dar
+        }, 1800);
+    })
+    /*Calcular el alto de la pantalla*/
 function configurarCSS(screen){
 	$(".full-page").css({"min-height":screen});
 }
