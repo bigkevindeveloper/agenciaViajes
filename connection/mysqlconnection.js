2333a2200a23ec31039a2f7a69faa7e96eec7fb6
Raul De la Paz <rauldelapaznavarro@gmail.com>
2018-12-27 11:48:32 +0100

    Eliminar estilos en html

--- connection/mysqlconnection.js
+++ connection/mysqlconnection.js
@@ -3,7 +3,7 @@ const Mysql = require('mysql');
 class mysqlConnection {
     static getConnection() {
         return Mysql.createConnection({
-            // Datos de conexión
+            // Datos de conexiï¿½n
             host: 'localhost',
             user: 'root',
             password: 'mysql',
